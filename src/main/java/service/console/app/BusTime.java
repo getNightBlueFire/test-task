0346828597714872f9323d1c86ffa3ef512f2b8f
Stonewall1 <impression97p@gmail.com>
2022-05-02 16:48:54 +0300

    Result added

--- src/main/java/service/console/app/BusTime.java
+++ src/main/java/service/console/app/BusTime.java
@@ -28,3 +28,14 @@ public class BusTime {
 	}
 
 }
+class Result{
+	public static void main(String[] args) {
+		double x = (100 *Math.random());
+		if(x < 50){
+			System.out.println("Wait for your bus");
+		}
+		else if(x >= 50){
+			System.out.println("Your bus has arrived");
+		}
+	}
+}
