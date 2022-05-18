243f8fa58f4de98872b136c22fe7b92e526006d5
Dmitry Hotko <d.hotko@invento.by>
2022-05-18 19:30:54 +0300

    fix README.ches

--- src/main/java/service/console/app/ConsoleApp.java
+++ src/main/java/service/console/app/ConsoleApp.java
@@ -1,132 +1,266 @@
 package service.console.app;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class ConsoleApp {
+    public static void main(String[] args) {
+        String[][] minerMap = new String[10 + 2][10 + 2];
 
-	public static void main(String[] args) {
+        outConsole(out(minerMap));
 
-		String input = args[0];
+    }
 
-		// String input = "C:/Users/Administrator/Desktop/input.txt";
-		String output = input.replace("input", "output");
-		List<Route> list = getInputFile(input);
-		sortList(list);
-		postProcessCollections(list);
-		writeToOutputFile(list, output);
+    private static void getMapMiner(String[][] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            for (int j = 1; j < array.length - 1; j++) {
+                if (!array[i][j].equals("x"))
+                    array[i][j] = isNearBomb(i, j, array);
+            }
+        }
+    }
 
-	}
+    private static String isNearBomb(int a, int b, String[][] array) {
+        int c = 0;
+        if (array[a + 1][b + 1].equals("x"))
+            c++;
+        if (array[a + 1][b].equals("x"))
+            c++;
+        if (array[a + 1][b - 1].equals("x"))
+            c++;
 
-	private static void sortList(List<Route> list) {
-		list.sort(new Comparator<Route>() {
+        if (array[a][b + 1].equals("x"))
+            c++;
+        if (array[a][b - 1].equals("x"))
+            c++;
+        if (array[a - 1][b + 1].equals("x"))
+            c++;
+        if (array[a - 1][b].equals("x"))
+            c++;
+        if (array[a - 1][b - 1].equals("x"))
+            c++;
+        if (c == 0) return " ";
+        return String.valueOf(c);
+    }
 
-			@Override
-			public int compare(Route o1, Route o2) {
-				// TODO Auto-generated method stub
+    private static void getRandomMines(String[][] array) {
+        Random random = new Random();
+        int[] ox = new int[10];
+        int[] oy = new int[10];
+        for (int i = 0; i < 10; i++) {
+            ox[i] = random.nextInt(10) + 1;
+        }
+        for (int i = 0; i < 10; i++) {
+            oy[i] = random.nextInt(10) + 1;
+        }
 
-				if (o1.getStart().getHour() > o2.getStart().getHour()) {
-					return 1;
-				}
-				if (o1.getStart().getHour() < o2.getStart().getHour()) {
-					return -1;
-				}
-				if (o1.getStart().getMinutes() > o2.getStart().getMinutes()) {
-					return 1;
-				}
-				if (o1.getStart().getMinutes() < o2.getStart().getMinutes()) {
-					return -1;
-				}
-				if (o1.getFinish().getMinutes() > o2.getFinish().getMinutes()) {
-					return -1;
-				}
-				if (o1.getFinish().getMinutes() < o2.getFinish().getMinutes()) {
-					return 1;
-				}
-				if (o1.getRouteTime() > o2.getRouteTime()) {
-					return 1;
-				}
-				if (o1.getRouteTime() < o2.getRouteTime()) {
-					return -1;
-				}
-				return 0;
-			}
+        for (int i = 0; i < 10; i++) {
+            array[ox[i]][oy[i]] = "x";
+        }
+    }
 
-		});
-	}
+    private static String[][] out(String[][] array) {
+        getMines(array);
+        getRandomMines(array);
+        getMapMiner(array);
+        String[][] result = new String[10][10];
+        for (int i = 1; i < array.length - 1; i++) {
+            for (int j = 1; j < array.length - 1; j++) {
+                result[i - 1][j - 1] = array[i][j];
+            }
+        }
+        return result;
+    }
 
-	private static List<Route> getInputFile(String path) {
-		List<Route> list = new ArrayList<Route>();
-		try (BufferedReader b = new BufferedReader(new FileReader(new File(path)));) {
-			String readLine = "";
-			while ((readLine = b.readLine()) != null) {
-				String company = readLine.split(" ")[0];
-				int hs = Integer.valueOf(readLine.split(" ")[1].split(":")[0]);
-				int ms = Integer.valueOf(readLine.split(" ")[1].split(":")[1]);
-				int hf = Integer.valueOf(readLine.split(" ")[2].split(":")[0]);
-				int mf = Integer.valueOf(readLine.split(" ")[2].split(":")[1]);
-				Route ro = new Route(company, new BusTime(hs, ms), new BusTime(hf, mf));
-				list.add(ro);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+    private static void outConsole(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-	private static void postProcessCollections(List<Route> list) {
-
-		for (Route route : list) {
-			if (route.getRouteTime() > 60) {
-				list.remove(route);
-			}
-		}
-		for (int i = 0; i < list.size() - 2; ++i) {
-			Route t = list.get(i);
-			for (int j = i + 1; j < list.size() - 1; ++j) {
-				if (t.getRouteTime() == list.get(j).getRouteTime()
-						&& t.getStart().getBusTime().equals(list.get(j).getStart().getBusTime())
-						&& t.getFinish().getBusTime().equals(list.get(j).getFinish().getBusTime())) {
-					list.remove(j);
-				}
-				if (t.getStart().getBusTime().equals(list.get(j).getStart().getBusTime())) {
-					list.remove(j);
-				}
-				if (t.getFinish().getBusTime().equals(list.get(j).getFinish().getBusTime())) {
-					list.remove(j - 1);
-				}
-			}
-		}
-	}
-
-	private static void writeToOutputFile(List<Route> list, String path) {
-		try (FileWriter fw = new FileWriter(path)) {
-			for (Route route : separateByCompanyName(list, "Posh")) {
-				fw.write(route.toOutput());
-			}
-			fw.write("\r\n");
-			for (Route route : separateByCompanyName(list, "Grotty")) {
-				fw.write(route.toOutput());
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static List<Route> separateByCompanyName(List<Route> list, String name) {
-		List<Route> listOfType = new ArrayList<>();
-		for (Route route : list) {
-			if (route.getBusCompany().contentEquals(name))
-				listOfType.add(route);
-		}
-		return listOfType;
-	}
+    private static void getMines(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = " ";
+            }
+        }
+    }
 
 }
+//public class ConsoleApp {
+//    public static void main(String[] args) {
+//        //chessGame();
+//        String[][] arr = new String[8][8];
+//        fillChess(arr);
+//        fillBoardStartPosition(arr);
+//        int count_move = 0;
+//        do {
+//            count_move++;
+//            findMoveForWhite(arr);
+//            outChess(arr);
+//            doMove(arr);
+//            System.out.println();
+//            findMoveForBlack(arr);
+//            outChess(arr);
+//            doMove(arr);
+//            System.out.println();
+//            if (count_move>2){
+//                break;
+//            }
+//        } while (true);
+//
+//    }
+//
+//    private static void doMove(String[][] arr) {
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//        String from = line.split("/")[0];
+//        String to = line.split("/")[1];
+//        move(arr, from, to);
+//    }
+//
+//    private static void move(String[][] arr, String from, String to){
+//        int x = Integer.parseInt(from.split("")[0])-1;
+//        int y = Integer.parseInt(from.split("")[1])-1;
+//        int x2 = Integer.parseInt(to.split("")[0])-1;
+//        int y2 = Integer.parseInt(to.split("")[1])-1;
+//        String temp = arr[x2][x2];
+//        arr[x2][y2] = arr[x][y];
+//        arr[x][y] = temp;
+//    }
+//
+//    private static void findMoveForWhite(String[][] arr) {
+//        for (int i = 1; i < arr.length; i++) {
+//            for (int j = 1; j < arr.length; j++) {
+//                if ((i + j) % 2 == 0 && (arr[i][j].equalsIgnoreCase("#")) && ((arr[i - 1][j - 1].equalsIgnoreCase(
+//                        "c")))) {
+//                    arr[i][j] = "x";
+//                }
+//            }
+//        }
+//    }
+//
+//    private static void findMoveForBlack(String[][] arr) {
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length - 1; j++) {
+//                if ((i + j) % 2 == 0 && (arr[i][j].equalsIgnoreCase("#")) && ((arr[i + 1][j + 1].equalsIgnoreCase(
+//                        "o")))) {
+//                    arr[i][j] = "z";
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    private static void fillBoardStartPosition(String[][] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                if ((i == 0 || i == 1 || i == 2) & ((i + j) % 2 == 0)) {
+//                    arr[i][j] = "c";
+//                }
+//                if ((i == 7 || i == 6 || i == 5) & ((i + j) % 2 == 0)) {
+//                    arr[i][j] = "o";
+//                }
+//            }
+//        }
+//    }
+//
+//    private static void chessGame() {
+//        //test();
+//        //if ((Math.abs(i - a) == Math.abs(j - b) ) || (Math.abs(i - a) == 0 || Math.abs(j - b) == 0)) { ферзь
+//        //if ((Math.abs(i - a) == 1 && Math.abs(j - b) == 1) || (Math.abs(i - a) == 1 && Math.abs(j - b) == 0) || (Math.abs(i - a) == 0 && Math.abs(j - b) == 1)) { король
+//        //if (Math.abs(i - a) == 0 || Math.abs(j - b) == 0) { //ладья
+//        //if (((Math.abs(i - a) == (Math.abs(j - b))))) { //слон
+//        //if (Math.abs(i - a) == 2 && Math.abs(j - b) == 1 || Math.abs(i - a) == 1 && Math.abs(j - b) == 2) //конь
+//        String[][] arr = new String[8][8];
+//        fillChess(arr);
+//
+//        int a = 2;
+//        int b = 1;
+//
+//        String[][] test = new String[5][5];
+//        fillChess1(test, a, b);
+//
+//        for (int i = 0; i < arr.length; i++)
+//            for (int j = 0; j < arr.length; j++)
+//                if ((Math.abs(i - a) == Math.abs(j - b)) || (Math.abs(i - a) == 0 || Math.abs(j - b) == 0)) {
+//                    if (arr[i][j] != "K")
+//                        arr[i][j] = "0";
+//                }
+//        arr[a][b] = "K";
+//
+//        a = 5;
+//        b = 5;
+//        for (int i = 0; i < arr.length; i++)
+//            for (int j = 0; j < arr.length; j++)
+//                if ((Math.abs(i - a) == Math.abs(j - b)) || (Math.abs(i - a) == 0 || Math.abs(j - b) == 0)) {
+//                    if (arr[i][j] != "K")
+//                        arr[i][j] = "0";
+//                }
+//
+//        arr[a][b] = "K";
+//
+//        //outChess(test);
+//        outChess(arr);
+//    }
+//
+//    private static void fillChess(String[][] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                arr[i][j] = "#";
+//            }
+//        }
+//    }
+//
+//    private static void fillChess1(String[][] arr, int a, int b) {
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                arr[i][j] = String.valueOf((i - a - arr.length / 2) + (j - b - arr.length / 2));
+//            }
+//        }
+//    }
+//
+//    private static void outChess(String[][] arr) {
+//        for (String[] strings : arr) {
+//            for (int j = 0; j < arr.length; j++) {
+//                System.out.print(strings[j] + "  ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//    private static void test() {
+//        int[] array0 = new int[3];
+//        int[] array1 = new int[3];
+//        if (isOne(array0) & isTwo(array1)) {
+//            System.out.println("Как заполнены массивы?");
+//            System.out.println(Arrays.toString(array0));
+//            System.out.println(Arrays.toString(array1));
+//        } else {
+//            System.out.println(Arrays.toString(array0));
+//            System.out.println(Arrays.toString(array1));
+//        }
+//    }
+//
+//    private static boolean isTwo(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = i + 10;
+//        }
+//        return true;
+//    }
+//
+//    private static boolean isOne(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (i + 1) * 2;
+//        }
+//        return false;
+//    }
+//}
