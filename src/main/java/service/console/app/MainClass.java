421f24e8732f69fe4a9eb3bf792cd4857a3fa68b
a.a.pligina <87080361+AnastasiaPlg@users.noreply.github.com>
2022-05-24 08:32:10 +0300

    Update MainClass.java

--- src/main/java/service/console/app/MainClass.java
+++ src/main/java/service/console/app/MainClass.java
@@ -7,17 +7,17 @@ import static service.console.app.DataBaseImitation.priorbank;
 
 public class MainClass {
     public static void main(String[] args) {
-        Person gay = new Person("Ivan", "Ivanov");
-        CreditCard priorbankCreditCard = gay.requestCreditCard(priorbank);
-        CreditCard belarusbankCreditCard = gay.requestCreditCard(belarusbank);
+        Person guy = new Person("Ivan", "Ivanov");
+        CreditCard priorbankCreditCard = guy.requestCreditCard(priorbank);
+        CreditCard belarusbankCreditCard = guy.requestCreditCard(belarusbank);
 
-        gay.addMoneyToCard(priorbankCreditCard.number, 1000);
-        gay.addMoneyToCard(belarusbankCreditCard.number, 650);
-        gay.transferMoneyFromCardToCard(priorbankCreditCard.number, belarusbankCreditCard.number, 100);
-        gay.payMoneyFromCard(priorbankCreditCard.number, 50);
-        gay.payMoneyFromCard(priorbankCreditCard.number, 150);
-        gay.addMoneyToCard(priorbankCreditCard.number, 140);
-        gay.transferMoneyFromCardToCard(belarusbankCreditCard.number, priorbankCreditCard.number, 20);
+        guy.addMoneyToCard(priorbankCreditCard.number, 1000);
+        guy.addMoneyToCard(belarusbankCreditCard.number, 650);
+        guy.transferMoneyFromCardToCard(priorbankCreditCard.number, belarusbankCreditCard.number, 100);
+        guy.payMoneyFromCard(priorbankCreditCard.number, 50);
+        guy.payMoneyFromCard(priorbankCreditCard.number, 150);
+        guy.addMoneyToCard(priorbankCreditCard.number, 140);
+        guy.transferMoneyFromCardToCard(belarusbankCreditCard.number, priorbankCreditCard.number, 20);
         priorbankCreditCard.bank.createAccountStatement();
     }
 }
